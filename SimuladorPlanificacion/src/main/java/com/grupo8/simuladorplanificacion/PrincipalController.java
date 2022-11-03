11a5cf38399c7a177cf2b5e358527578744c88f0
Alexandr-ml <68412919+Alexandr-ml@users.noreply.github.com>
2022-11-03 11:23:32 -0600

    Adicion de controladores
    Se añadieron los controlas a la vista principal

--- SimuladorPlanificacion/src/main/java/com/grupo8/simuladorplanificacion/PrincipalController.java
+++ SimuladorPlanificacion/src/main/java/com/grupo8/simuladorplanificacion/PrincipalController.java
@@ -1,12 +1,19 @@
 package com.grupo8.simuladorplanificacion;
 
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 
-public class PrincipalController {
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class PrincipalController implements Initializable {
 
     @FXML
     Button btnEjecutarSimulacion,btnVerEstadisticas;
@@ -20,4 +27,39 @@ public class PrincipalController {
     @FXML
     TextField txtListadoPeticiones;
 
-}
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        ArrayList<String> listadoAlgoritmos = new ArrayList<>();
+        listadoAlgoritmos.add("FCFS");
+        listadoAlgoritmos.add("SSTF");
+        listadoAlgoritmos.add("SCAN");
+        listadoAlgoritmos.add("C-SCAN");
+        listadoAlgoritmos.add("LOOK");
+        listadoAlgoritmos.add("C-LOOK");
+
+        cbAlgoritmos.setItems(FXCollections.observableArrayList(listadoAlgoritmos));
+
+        //Hacer funcion que traslade los puntos 
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        gc.setLineWidth(2);
+        double ancho = canvas.getWidth();
+        double alto = canvas.getHeight();
+
+        gc.strokeLine(20,20,20,alto);
+        gc.strokeLine(20,20,alto,20);
+
+
+        for(int i = 1 ;i<=10;i++){
+            gc.strokeLine(i*40+20,20,i*40+20,30);
+            gc.fillText(String.valueOf(i*10),i*40+10,10);
+        }
+
+        for(int i = 1 ;i<=10;i++){
+            gc.strokeLine(20,i*40+20,30,i*40+20);
+            gc.fillText(String.valueOf(i*10),0,i*40+25);
+        }
+
+
+
+    }
+    }
