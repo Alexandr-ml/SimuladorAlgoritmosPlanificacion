a773af7ac185b05e883bb893e52de7a2f16773b1
Claudia María Argueta Caneza <91162449+ClaudiaArguetaCaneza@users.noreply.github.com>
2022-11-14 21:02:47 -0600

    Vista Estadisticas

--- SimuladorPlanificacion/src/main/java/com/grupo8/simuladorplanificacion/EstadisticasController.java
+++ SimuladorPlanificacion/src/main/java/com/grupo8/simuladorplanificacion/EstadisticasController.java
@@ -1,11 +1,26 @@
 package com.grupo8.simuladorplanificacion;
 
+import com.grupo8.algoritmos.AbstractAlgoritmo;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
+
 public class EstadisticasController implements Initializable {
+
+    @FXML
+    TextField txtListPeticiones, txtnumpistas, txtresultado;
+    @FXML
+    TableView tableFCFS, tableSSTF, tableSCAN, tableCSCAN, tableLOOK, tableCLOOK;
+    @FXML
+    TextField txtFCFS, txtSSTF, txtSCAN, txtCSCAN, txtLOOK, txtCLOOK;
+
 
 
 
@@ -13,4 +28,6 @@ public class EstadisticasController implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
 
     }
+
 }
+
